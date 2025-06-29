035758f1f4e54d468d6be61e40ac9e7b3bc040af
ZiadHamad2252009 <reda.hamad.reda@gmail.com>
2025-06-29 14:06:39 +0200

    Add files via upload

--- session 3.py
+++ session 3.py
@@ -1,7 +1,5 @@
-# print number from 1 to 21 
-for i in range(21):
-    print (i)
-numbers,divisible,by,3:("1,3,6,12,15,18")
-for number in numbers_divisible,by,3:
-     print("number")
+for number in range (1,21):
+    if number%3==0:
+     print("this number can be devided on 3 ;", number)
 
+           
